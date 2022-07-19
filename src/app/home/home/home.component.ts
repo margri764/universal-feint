13367626e0f08dc0ffd011266bc5971313e49e41
margri764 <62308899+margri764@users.noreply.github.com>
2022-07-19 08:37:42 -0300

    SEO implemented

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CardsService } from 'src/app/services/cards.service';
+import { Meta, Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-home',
@@ -12,7 +13,10 @@ export class HomeComponent implements OnInit {
   imgWidth : any;
 
   constructor(
-              private _card : CardsService
+              private _card : CardsService,
+              private meta: Meta,
+              private title : Title,
+
   ) { 
     this.featuresCards = this._card.getCards();
  
@@ -23,6 +27,11 @@ export class HomeComponent implements OnInit {
 
 
   ngOnInit(): void {
+
+    this.title.setTitle("Desarrollo Paginas web - Crear Sitios web y Apps")
+    this.meta.addTag({name:"description", content:"desarrollo web, crear sitios web, marketing digital, realico, la pampa, paginas web, app, aplicaciones web"});
+    this.meta.addTag({name:"keywords", content:"desarrollo web, sitios web, marketing digital, realico, la pampa, paginas web, app"});
+  
 
   }
 
