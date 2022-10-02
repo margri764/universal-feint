2182542b1ee2749a1423cd40d90fed3526d19534
margri764 <62308899+margri764@users.noreply.github.com>
2022-10-01 23:03:32 -0300

    tag manager-

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
 import { GoogleTagManagerService } from 'angular-google-tag-manager';
 
-// declare let gtag: (property: string, value: any, configs: any) => {};
-declare var gtag : any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
 
 
 
@@ -22,26 +18,19 @@ export class AppComponent  {
 )
 {
 
-  this.router.events.forEach(item => {
-    if (item instanceof NavigationEnd) {
-      const gtmTag = {
-        event: 'page',
-        pageName: item.url
-      };
-
-      this.gtmService.pushTag(gtmTag);
-    }
-  });
-
-
-  // this.router.events.subscribe(event => {
-  //   if (event instanceof NavigationEnd) {
-  //     gtag('config', environment.googleAnalyticsId, {
-  //       'page_path': event.urlAfterRedirects
-  //     });
-  //   }
-  // });
 
 
 }
+  ngOnInit(): void {
+    this.router.events.forEach(item => {
+      if (item instanceof NavigationEnd) {
+        const gtmTag = {
+          event: 'page',
+          pageName: item.url
+        };
+  
+        this.gtmService.pushTag(gtmTag);
+      }
+    });
+  }
 }
